b0d68a3165a7f3f783bd21b8d2f80901135f35cf
雾时之森 <ye8980@qq.com>
2020-05-08 01:16:11 +0800

    更新JQ版本以及样式

--- page-anime.php
+++ page-anime.php
@@ -8,22 +8,23 @@
 get_header(); ?>
 
 <meta name="referrer" content="never">
-<link href="https://cdn.jsdelivr.net/gh/Fog-Forest/bilibili@1.4/page-anime.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/Fog-Forest/bilibili@1.7/page-anime.min.css" rel="stylesheet">
 <div id="container" class="container" >
     <div class="page-header">
         <h1>我的追番
          <?php
+            $show_num = 11;  // 首次要展示番剧数目默认为12个
             require_once ("bilibili/bilibiliAnime.php");
             $bili=new bilibiliAnime('你的B站UID','你的B站Cookie');
             echo "<small>当前已追".$bili->sum."部，继续加油！</small></h1></div><div class=\"row\">";
-			//处理观看进度条
+			// 处理观看进度条
             function precentage($str1,$str2)
             {
                 if(is_numeric($str1) && is_numeric($str2)) return $str1/$str2*100;
                 elseif ($str1=="貌似还没有看呢~" || $str2=="还没开始更新呢~") return 0;
                 else return 100;
             }
-			//处理观看进度记录
+			// 处理观看进度记录
 			function record($str1,$str2)
 			{
 				if(is_numeric($str1) && is_numeric($str2) && $str1==$str2) return "已经追完了咯~";
@@ -34,22 +35,24 @@ get_header(); ?>
 			}
             for($i=0;$i<$bili->sum;$i++)
             {
-            	if($i > 11){//首次要展示番剧数目默认为12个
+            	if($i > $show_num){
             		$more = "more";
             	}
                 echo "<div class=\"bangumi-item col-md-4 col-lg-3 ".$more."\"><a class=\"no-line bangumi-link\" href=\"https://www.bilibili.com/bangumi/play/ss".$bili->season_id[$i]."/ \" target=\"_blank\"><div class=\"bangumi-banner\"><img src=\"".$bili->image_url[$i]."\"><div class=\"bangumi-des\"><p>".$bili->evaluate[$i]."</p></div></div><div class=\"bangumi-content\"><h3 class=\"bangumi-title\">".$bili->title[$i]."</h3><div class=\"bangumi-progress\" style=\"width:100%\"><div class=\"bangumi-progress-bar\" style=\"width:".precentage($bili->progress[$i],$bili->total[$i])."%\"></div></div><div class=\"bangumi-progress-num\">进度：".record($bili->progress[$i],$bili->total[$i])."</div></div></a></div>";
             }
+            if($bili->sum > $show_num){
+                echo "<div class=\"showall\">. Show All .</div>";
+            }
         ?>
     </div>
-    <center><div class="showall">. Show All .</div></center>
 </div>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
-<script type="text/javascript">//收缩展示
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js"></script>
+<script type="text/javascript">  // 收缩展示
 $(document).ready(function(){
 	$(".more").hide();
 	$(".showall").click(function(){
-		$(".more").show(1000);
-		$(".showall").hide();
+		$(".more").fadeIn();
+		$(".showall").text("真的已经到头了哦~");
 	});
 });</script>
 
--- page-movie.php
+++ page-movie.php
@@ -8,32 +8,36 @@
 get_header(); ?>
 
 <meta name="referrer" content="never">
-<link href="https://cdn.jsdelivr.net/gh/Fog-Forest/bilibili@1.5/page-movie.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/Fog-Forest/bilibili@1.7/page-movie.min.css" rel="stylesheet">
 <div id="container" class="container" >
     <div class="page-header">
         <h1>我的追剧
          <?php
+			$show_num = 11;  // 首次要展示番剧数目默认为12个
             require_once ("bilibili/bilibiliMovie.php");
             $bili=new bilibiliMovie('你的B站UID','你的B站Cookie');
             echo "<small>当前已追".$bili->sum."部，继续加油！</small></h1></div><div class=\"row\">";
             for($i=0;$i<$bili->sum;$i++)
             {
-            	if($i > 11){//首次要展示番剧数目默认为12个
+            	if($i > $show_num){
             		$more = "more";
             	}
                 echo "<div class=\"bangumi-item col-md-4 col-lg-3 ".$more."\"><a class=\"no-line bangumi-link\" href=\"https://www.bilibili.com/bangumi/play/ss".$bili->season_id[$i]."/ \" target=\"_blank\"><div class=\"bangumi-banner\"><img src=\"".$bili->image_url[$i]."\"><div class=\"bangumi-des\"><p>".$bili->evaluate[$i]."</p></div></div><div class=\"bangumi-content\"><h3 class=\"bangumi-title\">".$bili->title[$i]."</h3><div class=\"bangumi-progress\" style=\"width:100%\"></div><div class=\"bangumi-progress-num\">".$bili->progress[$i]."</div></div></a></div>";
             }
+			if($bili->sum > $show_num){
+			    echo "<div class=\"showall\">. Show All .</div>";
+			}
         ?>
     </div>
     <center><div class="showall">. Show All .</div></center>
 </div>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
-<script type="text/javascript">//收缩展示
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js"></script>
+<script type="text/javascript">  // 收缩展示
 $(document).ready(function(){
 	$(".more").hide();
 	$(".showall").click(function(){
-		$(".more").show(1000);
-		$(".showall").hide();
+		$(".more").fadeIn();
+		$(".showall").text("真的已经到头了哦~");
 	});
 });</script>
 

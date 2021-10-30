1172f820cfb0af309f56738414535144830a76f2
随意之光 <69178257+Suiyi-Light@users.noreply.github.com>
2021-10-30 14:52:31 +0800

    对后端 api 进行修改
    1. 添加了类型、追剧状态和完结状态三个标签

--- json/GetMovieData.php
+++ json/GetMovieData.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: https://blog.suiyil.cn');
+header('Access-Control-Allow-Methods: GET');
 $array = array();
 $limit = $_GET["limit"];
 $page = $_GET["page"];
@@ -8,6 +10,40 @@ if (empty($limit)) {
     die('limit 为必须参数');
 } elseif (empty($page)) {
     $page = 0;
+}
+
+//完结状态
+function finish($str1, $str2, $type)
+{
+    if (is_numeric($str1) && $str1 == 1 && $str2 == 1){
+        return "已完结";
+    } elseif (is_numeric($str1) && $str1 == 1 && $str2 == 0 ){
+        return "敬请期待";
+    } elseif (is_numeric($str1) && $str1 == 0 && $type == 电影 )
+ {
+        return "敬请期待";
+    } elseif (is_numeric($str1) && $str1 == 0)
+ {
+        return "更新中";
+    } else {
+        return "状态未知";
+    }
+}
+
+//追番状态
+function follow_status($str1)
+{
+    if (is_numeric($str1) && $str1 == 1) {
+        return "想看";
+    } elseif (is_numeric($str1) && $str1 == 2)
+ {
+        return "在看";
+    } elseif (is_numeric($str1) && $str1 == 3)
+ {
+        return "看过";
+    } else {
+        return "状态未知";
+    }
 }
 
 require_once("bilibiliAcconut.php");
@@ -28,6 +64,9 @@ for ($i = 0; $i < $total; $i++) {
     $array[$i]['image_url'] = $biliM->image_url[$pagenum];
     $array[$i]['evaluate'] = $biliM->evaluate[$pagenum];
     $array[$i]['id'] = $biliM->season_id[$pagenum];
+    $array[$i]['finish'] = finish($biliM->finish[$pagenum], $biliM->can_watch[$pagenum], $biliM->type[$pagenum]);
+    $array[$i]['follow_status'] = follow_status($biliM->follow_status[$pagenum]);
+    $array[$i]['type'] = $biliM->type[$pagenum];
     $pagenum++;
 }
 echo '{"total": ' . $total . ',"total_page": ' . $total_page . ', "limit": ' . $limit . ', "page": ' . $page . ', "data":' . json_encode($array, true) . '}';
